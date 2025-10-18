c52c6e68ad4b6ab6fc46636d2893e8ce390f0c49
Felipe Santos dos Anjos <felipeanjos@192.168.0.5>
2025-10-18 18:26:32 -0300

    chore: project cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://automationexercise.com/',
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
